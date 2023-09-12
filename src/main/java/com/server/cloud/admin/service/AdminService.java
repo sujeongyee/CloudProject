701da101d9cd9f7caad76bfdcb84df70ddfe8be5
baeksy97 <baeksy1018@gmail.com>
2023-09-12 19:27:24 +0900

    백승용 23.09.12 07:27

--- src/main/java/com/server/cloud/admin/service/AdminService.java
+++ src/main/java/com/server/cloud/admin/service/AdminService.java
@@ -1,5 +1,23 @@
 package com.server.cloud.admin.service;
 
+import java.util.List;
+import java.util.Map;
+
+import com.server.cloud.command.NoticeVO;
+import com.server.cloud.pagenation.Criteria;
+
 public interface AdminService {
 
+	int getTotal();
+
+	List<NoticeVO>  getList(Criteria cri);
+	
+	
+		
+	
+	
+	
+	
+	
+	
 }
---
+++ src/main/java/com/server/cloud/s3/FileVO.java
@@ -0,0 +1,26 @@
+package com.server.cloud.s3;
+
+import java.sql.Timestamp;
+
+import com.server.cloud.command.CusVO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FileVO {
+
+	private String file_id;
+	private String file_name;
+	private String file_path;
+	private String file_type;
+	private Timestamp upload_date ;
+	private String user_id;
+	
+	
+}
