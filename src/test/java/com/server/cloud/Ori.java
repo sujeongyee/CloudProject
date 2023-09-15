f34cec1fc77e9804e542f6d7fe7f4dcf70144cca
sol0916 <yj0916lee00@gmail.com>
2023-09-15 15:52:52 +0900

    유저 프로젝트 신청

---
+++ src/test/java/com/server/cloud/Ori.java
@@ -0,0 +1,17 @@
+package com.server.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Ori {
+
+	
+	@Test
+	public void example() {
+		System.out.println(System.getenv());
+	}
+	
+
+
+}
